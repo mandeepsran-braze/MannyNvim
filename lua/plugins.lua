ccf0576f30cbddf0bdbea6727d12adfba37ae490
Manny <mandeep.sran@braze.com>
2025-05-14 12:09:59 -0400

    update: Adding statuscol plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -698,6 +698,28 @@ return {
       require('persistence').setup {}
     end,
   },
+  {
+    'luukvbaal/statuscol.nvim',
+    config = function()
+      -- local builtin = require("statuscol.builtin")
+      require('statuscol').setup {
+        -- configuration goes here, for example:
+        -- relculright = true,
+        -- segments = {
+        --   { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+        --   {
+        --     sign = { namespace = { "diagnostic/signs" }, maxwidth = 2, auto = true },
+        --     click = "v:lua.ScSa"
+        --   },
+        --   { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
+        --   {
+        --     sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
+        --     click = "v:lua.ScSa"
+        --   },
+        -- }
+      }
+    end,
+  },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
