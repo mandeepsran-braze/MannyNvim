408118a87930222266e3072d51960414461fe2e0
Manny <mandeep.sran@braze.com>
2026-04-07 13:14:03 -0400

    fix: pS opens Lazy UI instead of running clear

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -106,7 +106,7 @@ return {
       -- Plugins
       { "<leader>pi", "<cmd>Lazy install<cr>",  desc = "Install" },
       { "<leader>ps", "<cmd>Lazy sync<cr>",     desc = "Sync" },
-      { "<leader>pS", "<cmd>Lazy clear<cr>",    desc = "Status" },
+      { "<leader>pS", "<cmd>Lazy<cr>",            desc = "Status" },
       { "<leader>pc", "<cmd>Lazy clean<cr>",    desc = "Clean" },
       { "<leader>pu", "<cmd>Lazy update<cr>",   desc = "Update" },
       { "<leader>pp", "<cmd>Lazy profile<cr>",  desc = "Profile" },
